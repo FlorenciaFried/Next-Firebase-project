4a313ee909f428223e23e5041bc6faca94b5ef4b
FlorenciaFried <florencia.d.fried@gmail.com>
2020-05-03 18:52:21 -0300

    Header y su CSS listos

---
+++ pages/nuevoProducto.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout/Layout";
+
+const nuevoProducto = () => {
+  return (
+    <Layout>
+      <h1>Nuevo producto</h1>
+    </Layout>
+  );
+};
+
+export default nuevoProducto;
