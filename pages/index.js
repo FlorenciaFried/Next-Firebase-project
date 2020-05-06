fa2f380571a1be5053a076f93cac3c9a6d227cf7
FlorenciaFried <florencia.d.fried@gmail.com>
2020-05-06 20:10:32 -0300

    Cree todo los componentes para hacer que los productos se miestren en la pagina principal, estoy a mitad de camino para el routing dinamico

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,44 @@
-import React from "react";
-import Layout from '../components/layout/Layout';
+import React, { useEffect, useState, useContext } from "react";
+import Layout from "../components/layout/Layout";
+import DetallesProducto from "../components/layout/DetallesProducto";
+import { FirebaseContext } from "../firebase";
 
 const Home = () => {
-  return(
-    <Layout>
-      <h1>Inicio</h1>
-    </Layout>
+  const [productos, setProductos] = useState([]);
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    firebase.db
+      .collection("productos")
+      .orderBy("creado", "desc")
+      .onSnapshot(manejarSnapshpot);
+  }, []);
+
+  function manejarSnapshpot(snapshot) {
+    const productos = snapshot.docs.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      };
+    });
+
+    setProductos(productos);
+  }
+
+  return (
+    <div>
+      <Layout>
+        <div className="listado-productos">
+          <div className="contenedor">
+            <ul className="bg-white">
+              {productos.map((producto) => (
+                <DetallesProducto key={producto.id} producto={producto} />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </Layout>
+    </div>
   );
 };
 
